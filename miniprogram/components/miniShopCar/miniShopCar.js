6d3b0275caa19ec2a3e35e302fc3dd237a4359da
你猜猜我是谁 <38216943+islanddddddd@users.noreply.github.com>
2021-08-04 22:45:53 +0800

    添加底栏购物车

--- miniprogram/components/miniShopCar/miniShopCar.js
+++ miniprogram/components/miniShopCar/miniShopCar.js
@@ -1,23 +1,70 @@
 // components/miniShopCar/miniShopCar.js
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+    /**
+     * 组件的属性列表
+     */
+    properties: {},
 
-  },
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        dataSource: null,
+        num: 0,
+        price: 0,
+        marketPrice: 0,
+        timer: null,
+    },
+    // 生命周期
+    lifetimes: {
+        attached: function () {
+            let that = this;
+            that.getShopCarGoods();
+        },
+    },
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        getShopCarGoods: function () {
+            let that = this;
+            // console.log(222222222222);
+            let app = getApp();
+            var data = new Array();
+            for (var key in app.globalData.shopCarGoods) {
+                data.push(app.globalData.shopCarGoods[key]);
+            }
+            that.setData({
+                dataSource: data,
+            });
+            that.getGoodsNum();
+        },
+        getGoodsNum: function () {
+            let that = this;
 
-  },
+            let DS = that.data.dataSource;
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
+            let num = 0;
+            let price = 0;
+            let marketPrice = 0;
 
-  }
-})
+            for (var i = 0; i < DS.length; ++i) {
+                let buy = 0;
+                num += DS[i].buy;
+                buy = DS[i].buy;
+                price += DS[i].price * buy;
+                marketPrice += DS[i].marketPrice * buy;
+            }
+
+            price = price.toFixed(2);
+            marketPrice = marketPrice.toFixed(2);
+
+            that.setData({
+                num: num,
+                price: price,
+                marketPrice: marketPrice,
+            });
+        },
+    },
+});
--- miniprogram/pages/category/category.js
+++ miniprogram/pages/category/category.js
@@ -1,202 +1,213 @@
 Page({
-  data:{
-      "rightDataSource":[
-        {
-          category_name:'啤酒',//分类名称
-          thumb_url:'cloud://shybeejd-5gv8sqyv03b56093.7368-shybeejd-5gv8sqyv03b56093-1306511324/code.png', //展示图片
-          title:'青岛',//商品名
-          sale_count:5,
-          price:1499, //现在价格
-          marketPrice:2099, //原始价格
-          specification:'青岛优选',
-          stock:10, //库存数量
-             buy:0, //添加到购物车的数量
-          id:1 //商品id
-        }
-      ],
-      "leftDataSource":['啤酒','葡萄酒','白酒'],
-      allDataSouce:null,
-      "leftListSelectItem":0,
-      rightItemWidth:0,
-      envId:''
-  },
-     onLoad:function(options){
-    // 生命周期函数--监听页面加载
-    var app=getApp()
-    this.setData({
-      envId:app.globalData.envId
-    })
-    this.requestDataFromServe()
-    this.renderControl()
-  },
-  onReady:function(){
-    // 生命周期函数--监听页面初次渲染完成
-    
-  },
-  onShow:function(){
-    // 生命周期函数--监听页面显示
-    this.updataRightData()
-  },
-  onHide:function(){
-    // 生命周期函数--监听页面隐藏
-  },
-  onUnload:function(){
-    // 生命周期函数--监听页面卸载
-  },
-  onPullDownRefresh: function() {
-    // 页面相关事件处理函数--监听用户下拉动作
-  },
-  onReachBottom: function() {
-    // 页面上拉触底事件的处理函数
-  },
-    // 用户点击右上角分享
-  onShareAppMessage: function() {
-    return {
-      title: '酒运达', // 分享标题
-      desc: '酒运达，即刻达达，喝酒就上酒运达', // 分享描述
-      path: 'http://www.masyang.com' // 分享路径
-    }
-  },
-  // 渲染控制
-  renderControl:function(){
-    var app = getApp();
-    var width = app.globalData.systemInfo.windowWidth - 75.0;
-    this.setData({
-      rightItemWidth : width
-    })
-  },
-
-// 跳转到商品详情
-  pushGoodDetail:function(tap){
-    var index = tap.currentTarget.id;
-    var good = this.data.rightDataSource[index];
-    wx.navigateTo({
-      url: '../shopDetail/shopDetail?product_id='+ good.id + "&title=" + good.title,
-      success: function(res){
-      },
-      fail: function() {
-      },
-      complete: function() {
-      }
-    })
-  },
-   // 右侧列表被点击
-  rightListClick(par){
-    var index = parseInt(par.currentTarget.id);
-    this.setData({
-        leftListSelectItem:index,
-    })
-    this.updataRightData()
-  },
-  //更新右侧数据
-  updataRightData:function(){
-    if(this.data.allDataSource == null){
-      return;
-    }
-    var selectClassStr = this.data.leftDataSource[this.data.leftListSelectItem];
-    var selectData = new Array;
-    var app= getApp();
-    for(var index in this.data.allDataSource){
-      var good = this.data.allDataSource[index]; 
-      if(good.category_name == selectClassStr){
-        var tempGood = app.globalData.shopCarGoods[good.id];
-        if(tempGood != null){
-          good["buy"] = tempGood.buy;
-        }
-        selectData.push(good);
-      }
-    }
-    this.setData({
-        rightDataSource:selectData,
-    })
-  },
-// 减少商品
-  reduceImageClick(par){
-      var index = parseInt(par.currentTarget.id);
-      var data = this.data.rightDataSource[index];
-      if (data.buy > 0){
-        data.buy -= 1;
+    data: {
+        rightDataSource: [
+            //字符
+            {
+                category_name: "啤酒", //分类名称
+                thumb_url:
+                    "cloud://shybeejd-5gv8sqyv03b56093.7368-shybeejd-5gv8sqyv03b56093-1306511324/code.png", //展示图片
+                title: "青岛", //商品名
+                sale_count: 5,
+                price: 1499, //现在价格
+                marketPrice: 2099, //原始价格
+                specification: "青岛优选",
+                stock: 10, //库存数量
+                buy: 0, //添加到购物车的数量
+                id: 1, //商品id
+            },
+        ],
+        leftDataSource: ["啤酒", "葡萄酒", "白酒"], //字符
+        allDataSouce: null,
+        leftListSelectItem: 0, //字符
+        rightItemWidth: 0,
+        envId: "",
+    },
+    onLoad: function (options) {
+        // 生命周期函数--监听页面加载
+        var app = getApp();
         this.setData({
-            rightDataSource:this.data.rightDataSource
-        })
-      } 
-      var app = getApp();
-      app.reduceGoodFromShopCar(data)
+            envId: app.globalData.envId,
+        });
+        this.requestDataFromServe();
+        this.renderControl();
+    },
+    onReady: function () {
+        // 生命周期函数--监听页面初次渲染完成
+    },
+    onShow: function () {
+        // 生命周期函数--监听页面显示
+        this.updataRightData();
+    },
+    onHide: function () {
+        // 生命周期函数--监听页面隐藏
+    },
+    onUnload: function () {
+        // 生命周期函数--监听页面卸载
+    },
+    onPullDownRefresh: function () {
+        // 页面相关事件处理函数--监听用户下拉动作
+    },
+    onReachBottom: function () {
+        // 页面上拉触底事件的处理函数
+    },
+    // 用户点击右上角分享
+    onShareAppMessage: function () {
+        return {
+            title: "酒运达", // 分享标题
+            desc: "酒运达，即刻达达，喝酒就上酒运达", // 分享描述
+            path: "http://www.masyang.com", // 分享路径
+        };
+    },
+    // 渲染控制
+    renderControl: function () {
+        var app = getApp();
+        var width = app.globalData.systemInfo.windowWidth - 75.0;
+        this.setData({
+            rightItemWidth: width,
+        });
+    },
+
+    // 跳转到商品详情
+    pushGoodDetail: function (tap) {
+        var index = tap.currentTarget.id;
+        var good = this.data.rightDataSource[index];
+        wx.navigateTo({
+            url:
+                "../shopDetail/shopDetail?product_id=" +
+                good.id +
+                "&title=" +
+                good.title,
+            success: function (res) {},
+            fail: function () {},
+            complete: function () {},
+        });
+    },
+    // 右侧列表被点击
+    rightListClick(par) {
+        var index = parseInt(par.currentTarget.id);
+        this.setData({
+            leftListSelectItem: index,
+        });
+        this.updataRightData();
+    },
+    //更新右侧数据
+    updataRightData: function () {
+        if (this.data.allDataSource == null) {
+            return;
+        }
+        var selectClassStr =
+            this.data.leftDataSource[this.data.leftListSelectItem];
+        var selectData = new Array();
+        var app = getApp();
+        for (var index in this.data.allDataSource) {
+            var good = this.data.allDataSource[index];
+            if (good.category_name == selectClassStr) {
+                var tempGood = app.globalData.shopCarGoods[good.id];
+                if (tempGood != null) {
+                    good["buy"] = tempGood.buy;
+                }
+                selectData.push(good);
+            }
+        }
+        this.setData({
+            rightDataSource: selectData,
+        });
+    },
+    // 减少商品
+    reduceImageClick(par) {
+        var index = parseInt(par.currentTarget.id);
+        var data = this.data.rightDataSource[index];
+        if (data.buy > 0) {
+            data.buy -= 1;
+            this.setData({
+                rightDataSource: this.data.rightDataSource,
+            });
+        }
+
+        var app = getApp();
+        app.reduceGoodFromShopCar(data);
+
+        let myComponent = this.selectComponent("#myComponent");
+        myComponent.getShopCarGoods(); // 调用自定义组件中的方法
     },
     // 添加商品
-  addImageDidClick(par){
-    // console.log(par)
-    // 获取点击的商品索引
-    var index = parseInt(par.currentTarget.id);
-    // 在右侧数据里搜索对应索引的商品
-    var data = this.data.rightDataSource[index];
-    // 比较是否超过库存
-    if (data.buy < data.stock){
-      data.buy += 1;
-    }else{
-      wx.showToast({
-        title:"库存不足",
-          duration:2000,
-      });
-      return;
-    }
-    this.setData({
-      rightDataSource:this.data.rightDataSource
-    })
-    var app = getApp();
-    app.addGoodToShopCar(data)
-  },
+    addImageDidClick(par) {
+        // console.log(par)
+        // 获取点击的商品索引
+        var index = parseInt(par.currentTarget.id);
+        // 在右侧数据里搜索对应索引的商品
+        var data = this.data.rightDataSource[index];
+        // 比较是否超过库存
+        if (data.buy < data.stock) {
+            data.buy += 1;
+        } else {
+            wx.showToast({
+                title: "库存不足",
+                duration: 2000,
+            });
+            return;
+        }
+        this.setData({
+            rightDataSource: this.data.rightDataSource,
+        });
 
-// 获取数据
-  requestDataFromServe(){
-    var that = this;
-    wx.showLoading({
-      title: '',
-    })
-    console.log(this.data.envId)
-    // 从云函数获取数据
-    // wx.cloud.callFunction({
-    //   name: 'quickstartFunctions',
-    //   config: {
-    //     env: this.data.envId
-    //   },
-    //   data: {
-    //     type: 'getAllWine',
-    //     userInfo:{
-    //       openId:'ojVpU5XXun_ZlsmtOJKIJktiTNjc'
-    //     }
-    //   }
-    // }).then((res) => {
-    //   console.log("res.result")
-    //   console.log(res.result)
-    //   that.updateData(res.result);
-      wx.hideLoading()
-    // }).catch((e) => {
-    //   console.log(e)
-    // })
-  },
-  //更新数据
-  updateData:function(data){
-    //更新左侧数据
-    var leftData = new Array;
-    for (var key in data.category_contitions){
-      var str = data.category_contitions[key][0].category_name;
-      leftData.push(str);
-    }
-    this.setData({
-        leftDataSource : leftData,
-    })
-    //更新右侧数据
-    var allData = new Array;
-    for(var index in data.product_list){
-      var good = data.product_list[index]; 
-      console.log(data.product_list)
-      good.buy = 0;
-      allData.push(good);
-    }
-    this.setData({
-        allDataSource:allData
-    })
-    this.updataRightData();
-  },
-})
+        var app = getApp();
+        app.addGoodToShopCar(data);
+
+        let myComponent = this.selectComponent("#myComponent");
+        myComponent.getShopCarGoods(); // 调用自定义组件中的方法
+    },
+
+    // 获取数据
+    requestDataFromServe() {
+        var that = this;
+        wx.showLoading({
+            title: "",
+        });
+        console.log(this.data.envId);
+        // 从云函数获取数据
+        // wx.cloud.callFunction({
+        //   name: 'quickstartFunctions',
+        //   config: {
+        //     env: this.data.envId
+        //   },
+        //   data: {
+        //     type: 'getAllWine',
+        //     userInfo:{
+        //       openId:'ojVpU5XXun_ZlsmtOJKIJktiTNjc'
+        //     }
+        //   }
+        // }).then((res) => {
+        //   console.log("res.result")
+        //   console.log(res.result)
+        //   that.updateData(res.result);
+        wx.hideLoading();
+        // }).catch((e) => {
+        //   console.log(e)
+        // })
+    },
+    //更新数据
+    updateData: function (data) {
+        //更新左侧数据
+        var leftData = new Array();
+        for (var key in data.category_contitions) {
+            var str = data.category_contitions[key][0].category_name;
+            leftData.push(str);
+        }
+        this.setData({
+            leftDataSource: leftData,
+        });
+        //更新右侧数据
+        var allData = new Array();
+        for (var index in data.product_list) {
+            var good = data.product_list[index];
+            console.log(data.product_list);
+            good.buy = 0;
+            allData.push(good);
+        }
+        this.setData({
+            allDataSource: allData,
+        });
+        this.updataRightData();
+    },
+});
